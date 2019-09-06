5e7ce508cfd665624ed74087c727490f2eecc1b0
r-veiga <roberto.aprende.mucho@gmail.com>
2019-09-06 11:29:19 +0200

    2019.09.06 proj.persistencia3 primer commit

---
+++ abraham-persistencia-3/src/main/java/com/persistencia2/util/TiempoUtil.java
@@ -0,0 +1,19 @@
+package com.persistencia2.util;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TiempoUtil {
+	
+	private static final SimpleDateFormat yyyyMMdd = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+	public static Timestamp ahora() {
+		Timestamp ahora = new Timestamp((new Date()).getTime());
+		return ahora;
+	}
+	
+	public static String formateaFechaYyyyMMdd(Date fecha) {
+		return yyyyMMdd.format(fecha);
+	}
+}
